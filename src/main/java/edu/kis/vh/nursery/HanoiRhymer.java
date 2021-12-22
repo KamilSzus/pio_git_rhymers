265f6298e4b2c5d74202120f6b7fc673e057365b
KamilSzus <kamil-szustak1@wp.pl>
2021-12-22 20:12:14 +0100

    1.3.2 refactor code

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,17 +1,18 @@
 package edu.kis.vh.nursery;
 
+//kombinacja tych klawiszy alt + ← oraz alt + → pozwala przemieszczac sie pomiedzy otwartymi wczesniej plikami
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 }
